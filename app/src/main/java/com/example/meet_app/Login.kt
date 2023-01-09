41c7b6fea3a3b77a75d9abdf9cba696c5249a32f
Cephas Zulu <48860145+cazterk@users.noreply.github.com>
2023-01-09 11:34:54 +0200

    simple login layout

--- app/src/main/java/com/example/meet_app/Login.kt
+++ app/src/main/java/com/example/meet_app/Login.kt
@@ -3,7 +3,9 @@ package com.example.meet_app
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.Button
 import androidx.compose.material.Text
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
@@ -11,6 +13,12 @@ import androidx.navigation.NavController
 
 @Composable
 fun Login(navController:NavController, name: String?){
+    var emailUsernameFieldState by remember {
+        mutableStateOf("")
+    }
+    var passwordFieldState by remember{
+        mutableStateOf("")
+    }
 
     Column(
         verticalArrangement = Arrangement.Center,
@@ -20,10 +28,40 @@ fun Login(navController:NavController, name: String?){
             .fillMaxSize()
             .padding(16.dp)
     ) {
-        Text("This is the Login Screen")
-        Spacer(modifier = Modifier.height(16.dp))
+        Text("Welome To MeetApp")
+        Spacer(modifier = Modifier.height(20.dp))
+        OutlinedTextField(
+            value = emailUsernameFieldState,
+            label = {
+                Text("Email or Username")
+                    },
+            onValueChange ={
+                emailUsernameFieldState = it
+            },
+            singleLine = true,
+            modifier = Modifier
+                .width(360.dp)
+//                .height(40.dp)
+            )
+        Spacer(modifier = Modifier.height(20.dp))
+        OutlinedTextField(
+            value = passwordFieldState,
+            label = {
+                Text("Password")},
+            onValueChange ={
+                passwordFieldState = it
+            },
+            placeholder = { "Enter your password" },
+            singleLine = true,
+            modifier = Modifier
+                .width(360.dp)
+//                .height(40.dp)
+        )
+        Spacer(modifier = Modifier.height(20.dp))
         Button(
-            modifier = Modifier.width(150.dp),
+            modifier = Modifier
+                .width(350.dp)
+                .height(50.dp),
             onClick = {
                 navController.navigateUp()
             })
