bc7e0b0d0938aed748849ef3a01f699b42c4416a
Cephas Zulu <48860145+cazterk@users.noreply.github.com>
2023-01-09 13:09:32 +0200

    basic login functionality without auth

--- app/src/main/java/com/example/meet_app/Login.kt
+++ app/src/main/java/com/example/meet_app/Login.kt
@@ -1,24 +1,49 @@
 package com.example.meet_app
 
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Visibility
+import androidx.compose.material.icons.filled.VisibilityOff
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.focus.focusRequester
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 
+@OptIn(ExperimentalComposeUiApi::class)
 @Composable
 fun Login(navController:NavController, name: String?){
     var emailUsernameFieldState by remember {
         mutableStateOf("")
     }
-    var passwordFieldState by remember{
+    var passwordFieldState by remember {
         mutableStateOf("")
     }
+    var isVisible by remember {
+        mutableStateOf(false)
+    }
+
+    val focusRequester = remember {
+        FocusRequester()
+    }
+    val keyboardController = LocalSoftwareKeyboardController.current
 
     Column(
         verticalArrangement = Arrangement.Center,
@@ -26,9 +51,15 @@ fun Login(navController:NavController, name: String?){
 
         modifier = Modifier
             .fillMaxSize()
-            .padding(16.dp)
+            .padding(40.dp)
     ) {
-        Text("Welome To MeetApp")
+        Text(
+            text = "MeetApp",
+            fontWeight = FontWeight.SemiBold,
+            style= TextStyle(
+                fontSize = 25.sp
+            )
+        )
         Spacer(modifier = Modifier.height(20.dp))
         OutlinedTextField(
             value = emailUsernameFieldState,
@@ -40,9 +71,18 @@ fun Login(navController:NavController, name: String?){
             },
             singleLine = true,
             modifier = Modifier
-                .width(360.dp)
-//                .height(40.dp)
+                .fillMaxWidth(),
+            keyboardOptions = KeyboardOptions(
+                keyboardType = KeyboardType.Text,
+                imeAction = ImeAction.Next
+            ),
+            keyboardActions = KeyboardActions(
+                onNext = {
+                    focusRequester.requestFocus()
+                }
             )
+
+        )
         Spacer(modifier = Modifier.height(20.dp))
         OutlinedTextField(
             value = passwordFieldState,
@@ -54,19 +94,49 @@ fun Login(navController:NavController, name: String?){
             placeholder = { "Enter your password" },
             singleLine = true,
             modifier = Modifier
-                .width(360.dp)
-//                .height(40.dp)
+                .fillMaxWidth()
+                .focusRequester(focusRequester),
+            keyboardOptions = KeyboardOptions(
+                keyboardType = KeyboardType.Password
+            ),
+            keyboardActions = KeyboardActions(
+                onDone = {
+                    keyboardController?.hide()
+                }
+            ),
+            visualTransformation = if (isVisible){
+                    VisualTransformation.None
+
+            }else {
+                PasswordVisualTransformation()
+            },
+            trailingIcon =  {
+                IconButton(onClick = { isVisible = !isVisible}) {
+                    Icon(
+                        imageVector = if (isVisible){
+                        Icons.Filled.Visibility
+                        } else {
+                            Icons.Filled.VisibilityOff
+                        }, contentDescription = null
+                    )
+                }
+            }
+
+
         )
         Spacer(modifier = Modifier.height(20.dp))
         Button(
             modifier = Modifier
-                .width(350.dp)
+                .fillMaxWidth()
                 .height(50.dp),
+            enabled =
+                    emailUsernameFieldState.isNotEmpty()
+                    && passwordFieldState.isNotEmpty(),
             onClick = {
                 navController.navigateUp()
             })
         {
-            Text("Home")
+            Text("Login")
         }
     }
 }
