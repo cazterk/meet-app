b18acd71bd6f5489776e75834e36dcc56d582a02
Cephas Zulu <48860145+cazterk@users.noreply.github.com>
2023-01-11 13:59:32 +0200

    add profile features

--- app/src/main/java/com/example/meet_app/Profile.kt
+++ app/src/main/java/com/example/meet_app/Profile.kt
@@ -1,41 +1,124 @@
 package com.example.meet_app
 
+
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.Button
-import androidx.compose.material.Text
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.outlined.Edit
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.*
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
-import androidx.navigation.compose.rememberNavController
+
 
 @Composable
 fun Profile(navController: NavController, name: String?){
         val text by remember {
         mutableStateOf("")
     }
-
-    Column(
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally,
-
-
+    Column(//
         modifier = Modifier
             .fillMaxSize()
-            .padding(16.dp)
+
     ) {
-        Text("This is the profile screen")
-        Spacer(modifier = Modifier.height(16.dp))
-        Button(
-            modifier = Modifier.width(150.dp),
-            onClick = {
-            navController.navigateUp()
-        })
-        {
-            Text("Home")
+
+        TopAppBar(
+            title ={
+                Text(
+                     text = "Profile",
+                   )
+            },
+            backgroundColor = MaterialTheme.colors.background,
+            elevation = 4.dp,
+            navigationIcon = {
+                IconButton(
+                    onClick = { navController.navigateUp() }
+
+                ) {
+                    Icon(
+                        Icons.Filled.ArrowBack,
+                        contentDescription = "Back"
+                    )
+                }
+
+
+            }
+        )
+        Spacer(modifier = Modifier.height(5.dp))
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(16.dp)
+        ) {
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth(),
+                verticalAlignment = Alignment.CenterVertically
+            )  {
+                Image(
+                    modifier = Modifier
+                        .size(72.dp)
+                        .clip(shape = CircleShape),
+                    painter = painterResource(id = R.drawable.profile),
+                    contentDescription = "Image"
+                )
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.SpaceBetween,
+                    verticalAlignment = Alignment.CenterVertically
+                ){
+                    Column(
+                        modifier = Modifier
+                            .weight(weight = 3f, fill = false)
+                            .padding(start = 16.dp)
+                    ) {
+                        Text(
+                            text ="Cephas Zulu",
+                            style = TextStyle(
+                                fontSize = 22.sp
+                            ),
+                            maxLines = 1,
+                            overflow = TextOverflow.Ellipsis
+                        )
+                        Spacer(modifier = Modifier.height(2.dp))
+
+                        Text(
+                            text = "@cazterk",
+                            style = TextStyle(
+                                fontSize = 14.sp,
+                                color = Color.Gray,
+                                letterSpacing = (0.8).sp
+                            ),
+                            maxLines = 1,
+                            overflow = TextOverflow.Ellipsis
+                        )
+                    }
+                    IconButton(onClick = { /*TODO*/ }) {
+                        Icon(
+                            modifier = Modifier.size(24.dp),
+                            imageVector = Icons.Outlined.Edit,
+                            contentDescription = "Edit Details",
+                            tint = MaterialTheme.colors.primary
+                        )
+                    }
+                }
+            }
         }
+
     }
+
+   
 }
