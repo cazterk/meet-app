086c7242a12ebdf15ca236045a4bca0bc8268d47
Cephas Zulu <48860145+cazterk@users.noreply.github.com>
2023-01-26 11:03:43 +0200

    started home layout

--- app/src/main/java/com/example/meet_app/Home.kt
+++ app/src/main/java/com/example/meet_app/Home.kt
@@ -1,13 +1,22 @@
 package com.example.meet_app
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.Button
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.*
-import androidx.compose.ui.Modifier
 import androidx.compose.material.Text
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 
 @Composable
@@ -15,6 +24,74 @@ fun Home(navController: NavController){
     var text by remember {
         mutableStateOf("")
     }
+    var fonts = getFonts()
+    var name = "Cephas"
+    var username = "@cazterk"
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(color = Color.Gray)
+
+    ) {
+
+
+    Column( 
+        modifier = Modifier
+            .padding(32.dp)
+    ) {
+        Row (
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(120.dp)
+                .padding(top = 38.dp)
+                .background(color = Color.White),
+
+            verticalAlignment = Alignment.CenterVertically
+            ){
+            Spacer(modifier = Modifier.width(16.dp))
+              Image(
+                  modifier = Modifier
+                      .size(55.dp)
+                      .clip(shape = CircleShape)
+                      ,
+                  painter = painterResource(id = R.drawable.profile) , 
+                  contentDescription = "Image" 
+              )
+            Column(
+                modifier = Modifier
+                    .weight(weight = 3f, fill = false)
+                    .padding(start = 16.dp)
+            ) {
+                Text(
+                    text = "Welcome $name",
+                    style =  TextStyle(
+                        fontSize = 22.sp,
+                        fontFamily = fonts,
+                        fontWeight = FontWeight.SemiBold
+                    ),
+                    maxLines= 1,
+                    overflow = TextOverflow.Ellipsis
+                )
+                Text(
+                    text = "Get started with connections",
+                    style = TextStyle(
+                        fontSize = 14.sp,
+                        color = Color.Gray,
+                        letterSpacing = (0.8).sp,
+                        fontFamily = fonts,
+                        fontWeight = FontWeight.Normal
+                    ),
+                    maxLines = 1,
+                    overflow = TextOverflow.Ellipsis
+                )
+                
+            }
+            
+        }
+        
+    }
+    
     Column(
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally,
@@ -55,4 +132,5 @@ fun Home(navController: NavController){
         }
     }
 
+  }
 }
