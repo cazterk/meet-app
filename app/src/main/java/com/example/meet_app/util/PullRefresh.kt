c0fa50e822b1fa59f6c32265a9cf5a509f9e5546
Cephas Zulu <48860145+cazterk@users.noreply.github.com>
2023-09-02 13:54:48 +0200

    add pull to refresh across screen

--- app/src/main/java/com/example/meet_app/util/PullRefresh.kt
+++ app/src/main/java/com/example/meet_app/util/PullRefresh.kt
@@ -1,11 +1,63 @@
 package com.example.meet_app.util
 
-//@OptIn(ExperimentalMaterialApi::class)
-//@Composable
-//fun RememberPullRefreshState(
-//    refreshing: Boolean,
-//    onRefresh: () -> Unit,
-//    refreshThreshold: Dp = PullRefreshDefaults.RefreshThreshold,
-//    refreshingOffset: Dp = PullRefreshDefaults.RefreshingOffset
-//): PullRefreshState {
-//}
+import androidx.compose.foundation.gestures.detectVerticalDragGestures
+import androidx.compose.foundation.layout.Box
+import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.material.pullrefresh.PullRefreshIndicator
+import androidx.compose.material.pullrefresh.pullRefresh
+import androidx.compose.material.pullrefresh.rememberPullRefreshState
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableFloatStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.pointer.pointerInput
+import kotlinx.coroutines.launch
+
+@OptIn(ExperimentalMaterialApi::class)
+@Composable
+fun PullRefresh(
+    refreshing: Boolean,
+    onRefresh: () -> Unit,
+    modifier: Modifier = Modifier,
+    content: @Composable () -> Unit,
+) {
+
+
+    val refreshScope = rememberCoroutineScope()
+    var offsetY by remember { mutableFloatStateOf(0f) }
+    var isRefreshing by remember { mutableStateOf(refreshing) }
+
+    val state = rememberPullRefreshState(refreshing, onRefresh)
+    Box(
+        modifier = modifier
+            .pointerInput(Unit) {
+                detectVerticalDragGestures { _, dragAmount ->
+                    offsetY += dragAmount
+                    if (offsetY < 50f) {
+                        isRefreshing = true
+                        refreshScope.launch {
+
+                            onRefresh()
+
+
+                        }
+
+                    }
+                }
+            }
+            .pullRefresh(state)
+
+    ) {
+        content()
+        PullRefreshIndicator(
+            refreshing,
+            state,
+            Modifier.align(Alignment.TopCenter)
+        )
+    }
+}
